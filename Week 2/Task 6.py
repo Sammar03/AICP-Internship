1b9a7b6a43685b344ea1eaf564805d8a12152231
Sammar Ali <wsammar.ali@outlook.com>
2024-02-10 02:14:30 +0500

    Revert "Creation of the folder"
    This reverts commit 9e89a1350886614198a56f8e7b29602cc14940e2.

--- Week 2/Task 6.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd;
-import numpy as np;
-
-person = pd.read_csv("Datasets/people.csv", usecols=["First Name","Sex","Email","Phone","Job Title"], index_col=["Sex","Job Title"],skiprows=[1,5])
-print(person.head(5))
-
-# person.to_csv("Datasets/NewPeople.csv")
